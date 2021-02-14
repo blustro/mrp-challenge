eec80c19254cb94c4b4f31ffdef2aa0d9ba5f87d
blustro <brunolustro@gmail.com>
2021-02-14 21:55:50 +0000

    fixed router endpoint

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use('api/menu/users', usersRouter);
+app.use('/users', usersRouter);
 
 /**
  * Server Activation
--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -16,12 +16,81 @@ export const usersRouter = express.Router();
  * Controller Definitions
  */
 
-//GET users
+// GET users
+
 usersRouter.get('/', async (req: Request, res: Response) => {
   try {
     const users: User[] = await UserService.findAll();
 
     res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// GET users/:id
+
+usersRouter.get('/:id', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const user: User = await UserService.find(id);
+
+    if (user) {
+      return res.status(200).send(user);
+    }
+
+    res.status(404).send('user not found');
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// POST users
+
+usersRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const user: BaseUser = req.body;
+
+    const newuser = await UserService.create(user);
+
+    res.status(201).json(newuser);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// PUT users/:id
+
+usersRouter.put('/:id', async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const userUpdate: User = req.body;
+
+    const existinguser: User = await UserService.find(id);
+
+    if (existinguser) {
+      const updateduser = await UserService.update(id, userUpdate);
+      return res.status(200).json(updateduser);
+    }
+
+    const newuser = await UserService.create(userUpdate);
+
+    res.status(201).json(newuser);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+// DELETE users/:id
+
+usersRouter.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id, 10);
+    await UserService.remove(id);
+
+    res.sendStatus(204);
   } catch (e) {
     res.status(500).send(e.message);
   }
